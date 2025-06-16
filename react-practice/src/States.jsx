441dc8b67e7bcfb3bd6555de24ab8860eef35690
Vidisha Srivastava <vidishasrivastava2001@gmail.com>
2025-06-16 21:37:25 +0530

    usestate exmaple

--- react-practice/src/States.jsx
+++ react-practice/src/States.jsx
@@ -1,17 +1,18 @@
-const user=[
+import { useState } from 'react';
+
+export const States = ()=> {
+    const [users, setUsers] = useState([
     { name: "John", age: 30, city: "New York" },
     { name: "Jane", age: 25, city: "Los Angeles" },         
     { name: "Mike", age: 35, city: "Chicago" },
     { name: "Sara", age: 28, city: "Miami" },
     { name: "Tom", age: 40, city: "Seattle" },
-];
-
-export const States = ()=> {
+])
     return (
         <div>
             <h2>user details</h2>
             <ul>
-                {user.map((item, idx) => (
+                {users.map((item, idx) => (
                     <li key={item.name + idx}>
                         {item.name} is {item.age} years old and lives in {item.city}.
                     </li>
